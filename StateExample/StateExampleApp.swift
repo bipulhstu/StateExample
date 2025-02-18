d5d3cfa6d864b31ba80e919103841aa98a1054b3
Bipul Islam <bipulhstu@gmail.com>
2025-02-18 16:27:59 +0600

    Initial Commit

---
+++ StateExample/StateExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateExampleApp.swift
+//  StateExample
+//
+//  Created by Bipul Islam on 18/2/25.
+//
+
+import SwiftUI
+
+@main
+struct StateExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
